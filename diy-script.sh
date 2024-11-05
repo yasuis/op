f02c308633302292532c4edad82bd9eb7a778e55
yasuis <31124263+yasuis@users.noreply.github.com>
2024-11-06 05:21:21 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -53,9 +53,13 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 
 # Themes
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
 git clone --depth=1 -b https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+git clone -b master https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+git clone -b master https://github.com/jerrykuku/luci-theme-argon-config.git luci-app-argon-config
 git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
