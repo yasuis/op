cb25a3a35a4f6892ed431b89e9893469c1d66bac
yasuis <31124263+yasuis@users.noreply.github.com>
2024-12-04 11:46:07 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -14,7 +14,6 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/luci2/bin/config_gener
 sed -i 's/auto//' package/base-files/files/bin/config_generate
 
 # 移除要替换的包
-rm -rf staging_dir build_dir
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
