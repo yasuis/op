84124287e01c55efdbf5c09fd3894b15cd27cde3
yasuis <31124263+yasuis@users.noreply.github.com>
2024-12-23 21:39:11 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -28,6 +28,10 @@ rm -rf feeds/package/helloworld
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/ddns-go
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/sing-box
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -48,8 +52,8 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 # 科学上网插件
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git;main package/psw
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git;main package/pswcore
+git clone -b main https://github.com/xiaorouji/openwrt-passwall package/psw
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/pswcore
 git clone --depth=1 https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 
