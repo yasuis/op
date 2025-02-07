d6a37185cc801e3d1795fde9a58f0ac209114e3a
yasuis <31124263+yasuis@users.noreply.github.com>
2025-02-07 15:50:32 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -2,7 +2,7 @@
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/luci2/bin/config_generate
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
