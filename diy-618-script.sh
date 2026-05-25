b46d20af0055b032aee165871dd15710d05fef61
yasuis <31124263+yasuis@users.noreply.github.com>
2026-05-25 12:03:10 +0800

    Update diy-618-script.sh

--- diy-618-script.sh
+++ diy-618-script.sh
@@ -78,7 +78,7 @@ sed -i "s|ARMv8|ARMv8_lean|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # SmartDNS
 git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 -b master https://github.com/pymumu/smartdns package/smartdns
+git clone --depth=1 https://github.com/pymumu/smartdns package/smartdns
 
 # msd_lite
 git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
